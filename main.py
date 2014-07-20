3ec3e01b2d5035c32d596c30217b35db86fc019c
Phi Bui <Phi@PhiBuiLenovo.(none)>
2014-07-20 16:13:51 -0400

    start on onbutton press events

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ gun = pygame.image.load("gun.bmp")
 knife = pygame.image.load("knife.bmp")
 grenade = pygame.image.load("grenade.bmp")
 dynamite = pygame.image.load("dynamite.bmp")
-
 rect = gun.get_rect()
 
 button_tuples = [(gun, (GUN_COORDS)),
@@ -32,12 +31,44 @@ screen.fill(BACKGROUND_COLOR)
 for b in button_tuples:
     screen.blit(b[0], b[1])
 pygame.display.flip()
+player1_pattern = []
+player2_pattern = []
+set_number = 1;
 
 while 1:
-#     # Player 1's Turn
+	## Player 1's Turn
+
+	# Player 1 Records
      for event in pygame.event.get():
          if event.type == pygame.QUIT: sys.exit()
-# 
+         while pressed_count < set_number:
+ 	        if player_count < button_count:
+		        last_tur = false
+		        if event.type == pygame.MOUSEBUTTONDOWN:
+    	  	   	x, y = event.pos
+        	 	if x > GUN_COORDS[0] and x < GUN_COORDS[0] + 64 and
+         			y > GUN_COORDS[1] and y < GUN_COORDS[1] + 64:
+         			player1_pattern = player1_pattern.append("gun")
+         			player_count += 1
+
+         		if x > KNIFE_COORDS[0] and x < KNIFE_COORDS[0] + 64 and
+         			y > KNIFE_COORDS[1] and y < KNIFE_COORDS[1] + 64:
+         			player1_pattern = player1_pattern.append("knife")
+     				player_count += 1
+
+
+         		if x > GRENADE_COORDS[0] and x < GRENADE_COORDS[0] + 64 and
+         			y > GRENADE_COORDS[1] and y < GRENADE_COORDS[1] + 64:
+         			player1_pattern = player1_pattern.append("knife")
+         			player_count += 1
+
+         		if x > DYNAMIE_COORDS[0] and x < DYNAMIE_COORDS[0] + 64 and
+         			y > DYNAMIE_COORDS[1] and y < DYNAMIE_COORDS[1] + 64:
+         			player1_pattern = player1_pattern.append("knife")
+         			player_count += 1
+
+
+
 #     ballrect = ballrect.move(speed)
 #     if ballrect.left < 0 or ballrect.right > width:
 #         speed[0] = -speed[0]
